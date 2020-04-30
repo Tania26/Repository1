6f3ef573a92af06311cd3a131462035dbee3d71b
suga23 <Taurus1024@yandex.ru>
2020-05-01 00:34:30 +0300

    New task

---
+++ TestingTasks/TestingTasks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}

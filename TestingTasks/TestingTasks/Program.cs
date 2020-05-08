e177a5b4ccc40c5fdb5519d6ea8400cbb67ac3d2
suga23 <Taurus1024@yandex.ru>
2020-05-09 01:55:38 +0300

    lab

--- TestingTasks/TestingTasks/Program.cs
+++ TestingTasks/TestingTasks/Program.cs
@@ -4,17 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestingTasks
+namespace _3
 {
+    class MyArr
+    {
+        //Координаты точки в трёхмерном пространстве
+        public int x, y, z;
+
+        public MyArr(int x = 0, int y = 0, int z = 0)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //Перегружаем бинарный оператор +
+        public static MyArr operator +(MyArr obj1, MyArr obj2)
+        {
+            MyArr arr = new MyArr();
+            arr.x = obj1.x + obj2.x;
+            arr.y = obj1.y + obj2.y;
+            arr.z = obj1.z + obj2.z;
+            return arr;
+        }
+        public void Show()
+        {
+            Console.WriteLine(x + ", " + y + ", " + z);
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int[] arr = new int[5];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
+
+            MyArr Point1 = new MyArr(1, 12, -4);
+            MyArr Point2 = new MyArr(0, -3, 18);
+            Console.Write("Координаты первой точки Point1: ");
+            Point1.Show();
+            Console.Write("Координаты второй точки Point2: ");
+            Point2.Show();
+
+            MyArr Point3 = Point1 + Point2;
+            Console.WriteLine("\n Point1 + Point2 = " + Point3.x + ", " + Point3.y + ", " + Point3.z);
+            Console.Write("\n Point1 + Point2 = ");            
+            Point3.Show();
+
+            Console.ReadLine();
         }
     }
 }
